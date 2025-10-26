94d8e90ab54049b423a835ac5c9318dcefd503e2
David Peer <david.peer.1991@gmail.com>
2025-10-26 16:49:36 +0100

    Improvements

--- src/c/pebble-glitch-clock.c
+++ src/c/pebble-glitch-clock.c
@@ -1,20 +1,19 @@
 #include <pebble.h>
 
-// A pointer to the main window
+// A pointer to the main window and layers
 static Window *s_main_window;
-// A pointer to the time TextLayer
 static TextLayer *s_time_layer;
-// A pointer to the custom Glitch Layer
 static Layer *s_glitch_layer;
+static Layer *s_grid_layer;
 
 // Pointer for the animation AppTimer
 #define GRID_SIZE 15
 static AppTimer *s_animation_timer = NULL;
-#define ANIMATION_RATE_MS 300
-#define ANIMATION_DURATION_SECONDS 4
-#define NUM_ANIMATION_FRAMES (ANIMATION_DURATION_SECONDS * 1000 / ANIMATION_RATE_MS)
-#define NUM_GLITCHES 40
-#define NUM_GLITCHES_IF_ANIMATION_STOPPED 30
+#define ANIMATION_RATE_MS 5
+#define NUM_ANIMATION_FRAMES 50
+#define NUM_GLITCHES 180
+#define NUM_GLITCHES_IF_ANIMATION_STOPPED 60
+#define BORDER_THICKNESS 6
 static int current_animation_frame = 0;
 
 static int glitch_even[NUM_GLITCHES][4];
@@ -33,9 +32,10 @@ static void update_time();
 static void glitch_update_proc(Layer *layer, GContext *ctx);
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed);
 static void animation_timer_callback(void *data);
-static void start_animation_timer();
-static void stop_animation_timer();
+static void try_start_animation_timer();
+static void try_stop_animation_timer();
 static void generate_two_glitches();
+static void draw_grid(Layer *layer, GContext *ctx);
 
 
 // --- Update Time Function ---
@@ -51,11 +51,17 @@ static void update_time() {
 /**
  * @brief Stops the animation timer and resets the running flag.
  */
-static void stop_animation_timer() {
+static void try_stop_animation_timer() {
+    if(!s_is_animation_running) {
+        return; 
+    }
+
     if (s_animation_timer) {
+        APP_LOG(APP_LOG_LEVEL_DEBUG, "Stop animation timer");
         app_timer_cancel(s_animation_timer);
         s_animation_timer = NULL;
     }
+
     s_is_animation_running = false; 
     current_animation_frame = 0;
 }
@@ -63,15 +69,16 @@ static void stop_animation_timer() {
 /**
  * @brief Starts the animation timer if it's not already running.
  */
-static void start_animation_timer() {
-    if (s_is_animation_running) {
-        return; 
-    }
+static void try_start_animation_timer() {
+  if (s_is_animation_running) {
+      return; 
+  }
 
-    current_animation_frame = 0;
-    generate_two_glitches();
-    s_is_animation_running = true;
-    s_animation_timer = app_timer_register(ANIMATION_RATE_MS, animation_timer_callback, NULL);
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "Start animation timer");
+  current_animation_frame = 0;
+  generate_two_glitches();
+  s_is_animation_running = true;
+  s_animation_timer = app_timer_register(ANIMATION_RATE_MS, animation_timer_callback, NULL);
 }
 
 /**
@@ -82,12 +89,12 @@ static void animation_timer_callback(void *data) {
     if (!s_is_animation_running) {
         return;
     }
-
+    
     current_animation_frame++;
     if(current_animation_frame >= NUM_ANIMATION_FRAMES) {
       // Animation duration completed, stop the animation
       // but still draw the frame otherwise its not shown.
-      stop_animation_timer();
+      try_stop_animation_timer();
     }
     
     // Mark the layer as dirty to trigger a redraw
@@ -103,8 +110,8 @@ static void generate_glitch_for_pointer_to_array(int (*glitch_array)[4]) {
   for (int i = 0; i < NUM_GLITCHES; i++) {
     glitch_array[i][0] = rand() % bounds.size.w; // x
     glitch_array[i][1] = rand() % bounds.size.h; // y
-    glitch_array[i][2] = rand() % (bounds.size.w / 3) + 10; // width
-    glitch_array[i][3] = rand() % 2 + 1; // height
+    glitch_array[i][2] = rand() % (bounds.size.w / 5) + 10; // width
+    glitch_array[i][3] = 1; // height
   }
 }
 
@@ -120,33 +127,8 @@ static void glitch_update_proc(Layer *layer, GContext *ctx) {
 
   // 1. Draw the white border frame
   graphics_context_set_stroke_color(ctx, GColorWhite);
-  graphics_context_set_stroke_width(ctx, 5);
+  graphics_context_set_stroke_width(ctx, BORDER_THICKNESS);
   graphics_draw_rect(ctx, GRect(1, 1, bounds.size.w - 2, bounds.size.h - 2));
-
-  // 2. Draw a faint grid (scanlines)
-  graphics_context_set_stroke_color(ctx, GColorWhite);
-  graphics_context_set_stroke_width(ctx, 1);
-  
-  // Draw horizontal lines (every 10 pixels)
-  for (int y = 0; y < bounds.size.h; y += GRID_SIZE) {
-    if (y < 70 || y > bounds.size.h - 70) {
-        graphics_draw_line(ctx, GPoint(0, y), GPoint(bounds.size.w, y));
-        continue;
-    }
-    graphics_draw_line(ctx, GPoint(0, y), GPoint(20, y));
-    graphics_draw_line(ctx, GPoint(bounds.size.w - 20, y), GPoint(bounds.size.w, y));
-  }
-  
-  // Draw vertical lines (every 10 pixels)
-  for (int x = 0; x < bounds.size.w; x += GRID_SIZE) {
-    if(x < 20 || x > bounds.size.w - 20) {
-        graphics_draw_line(ctx, GPoint(x, 0), GPoint(x, bounds.size.h));
-        continue;
-    }
-    graphics_draw_line(ctx, GPoint(x, 0), GPoint(x, bounds.size.h / 2 - 20));
-    graphics_draw_line(ctx, GPoint(x, bounds.size.h / 2 + 20), GPoint(x, bounds.size.h));
-  }
-
 
 
   // Draw glitches
@@ -156,7 +138,7 @@ static void glitch_update_proc(Layer *layer, GContext *ctx) {
   int num_glitches_to_draw = s_is_animation_running ? NUM_GLITCHES : NUM_GLITCHES_IF_ANIMATION_STOPPED;
   for (int i = 0; i < num_glitches_to_draw; i++) {
     // Randomly choose color for more variation (white or black to obscure text)
-    if (rand() % 2 == 0) {
+    if (i % 2 == 0) {
       graphics_context_set_fill_color(ctx, GColorWhite);
     } else {
       // Use the background color (Black) to create a cut-off effect on the text
@@ -168,29 +150,62 @@ static void glitch_update_proc(Layer *layer, GContext *ctx) {
   }
 }
 
+static void draw_grid(Layer *layer, GContext *ctx) {
+  GRect bounds = layer_get_bounds(layer);
+  graphics_context_set_stroke_color(ctx, GColorWhite);
+  graphics_context_set_stroke_width(ctx, 1);
+  
+  // Draw horizontal lines (every 10 pixels)
+  for (int y = 0; y < bounds.size.h; y += GRID_SIZE) {
+    if (y < 70 || y > bounds.size.h - 70) {
+        graphics_draw_line(ctx, GPoint(0, y), GPoint(bounds.size.w, y));
+        continue;
+    }
+    graphics_draw_line(ctx, GPoint(0, y), GPoint(17, y));
+    graphics_draw_line(ctx, GPoint(bounds.size.w - 17, y), GPoint(bounds.size.w, y));
+  }
+  
+  // Draw vertical lines (every 10 pixels)
+  for (int x = 0; x < bounds.size.w; x += GRID_SIZE) {
+    if(x < 20 || x > bounds.size.w - 20) {
+        graphics_draw_line(ctx, GPoint(x, 0), GPoint(x, bounds.size.h));
+        continue;
+    }
+    graphics_draw_line(ctx, GPoint(x, 0), GPoint(x, bounds.size.h / 2 - 20));
+    graphics_draw_line(ctx, GPoint(x, bounds.size.h / 2 + 20), GPoint(x, bounds.size.h));
+  }
+}
 
 // --- Tick Handler ---
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
-  start_animation_timer();
-
+  try_start_animation_timer();
   update_time();
 }
 
 
 // --- Window Load/Unload Handlers ---
 static void main_window_appear(Window *window) {
-    start_animation_timer();
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "Window appear");
+    try_start_animation_timer();
 }
 
 static void main_window_load(Window *window) {
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "Window load");
   GRect bounds = layer_get_bounds(window_get_root_layer(window));
+
+  // 1. Create grid layer
+  s_grid_layer = layer_create(bounds);
+  // Draw the grid in its update procedure
+  layer_set_update_proc(s_grid_layer, draw_grid);
+  layer_add_child(window_get_root_layer(window), s_grid_layer);
 
   // 1. Create TextLayer for the time
   s_time_layer = text_layer_create(
       GRect(0, bounds.size.h / 2 - 21 - 6, bounds.size.w, bounds.size.h));
 
-  text_layer_set_background_color(s_time_layer, GColorBlack);
+  // Transparent background for the text layer
+  text_layer_set_background_color(s_time_layer, GColorClear);
   text_layer_set_text_color(s_time_layer, GColorWhite);
   text_layer_set_text(s_time_layer, "00:00"); 
   text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_LECO_42_NUMBERS));
@@ -230,11 +245,13 @@ static void init() {
     // Subscribe to MINUTE_UNIT (for time update/minute-start trigger) and SECOND_UNIT (for stop trigger)
     tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
     window_stack_push(s_main_window, true);
+
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "Finished init");
 }
 
 
 static void deinit() {
-  stop_animation_timer();
+  try_stop_animation_timer();
   window_destroy(s_main_window);
   tick_timer_service_unsubscribe();
 }
